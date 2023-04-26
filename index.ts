da8561bfeeb2383134a59e3f0128b3352b138183
Yura Dudin <dudinyv@yandex.ru>
2023-04-26 23:06:17 +0500

    create server

--- index.ts
+++ index.ts
@@ -11,30 +11,30 @@
 // server.listen(3000);
 
 
-import express from 'express'
-import { Request, Response } from 'express'
+// import express from 'express'
+// import { Request, Response } from 'express'
 
-const { PORT = 3000 } = process.env;
+// const { PORT = 3000 } = process.env;
 
-const app = express();
+// const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`)
+// })
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(
-    `
-    <html>
-    <body>
-        <p>Ответ на сигнал из далёкого космоса</p>
-    </body>
-    </html>
-    `
-  )
-})
+// app.get('/', (req: Request, res: Response) => {
+//   res.send(
+//     `
+//     <html>
+//     <body>
+//         <p>Ответ на сигнал из далёкого космоса</p>
+//     </body>
+//     </html>
+//     `
+//   )
+// })
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(404);
-  res.send('<h1>Страница не найдена</h1>');
-});
+// app.get('/', (req: Request, res: Response) => {
+//   res.status(404);
+//   res.send('<h1>Страница не найдена</h1>');
+// });
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
-// import http from "http"
-// import {IncomingMessage, ServerResponse} from "http"
-
-// const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-//   console.log(req.url);
-//   console.log(req.method)
-//   console.log(req.headers)
-// });
-
-
-// server.listen(3000);
-//express
-
-
-import express from 'express'
-
+import http from "http"
+import {IncomingMessage, ServerResponse} from "http"
 const { PORT = 3000 } = process.env;
 
-const app = express();
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  console.log(req.url);
+  console.log(req.method)
+  console.log(req.headers)
 
-app.listen(PORT, () => {
+
+  res.writeHead(200, {
+    'Content-type': 'text/plain; charset=utf8'
+  })
+
+  res.end;
+});
+
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 })
+
+
+// import express from 'express'
+
+// const app = express();
+
+
 
 
