87f8618108d046231735f602665216e812cc4a68
Yuriy <xgooglex@bk.ru>
2023-05-02 17:55:03 +0500

    fix controllers

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -18,28 +18,32 @@ export const getCards = async (req: Request, res: Response) => {
 
 //создаёт карточку
 export const createCard = async (req: ITestRequest, res: Response) => {
-  let data = req.body; //данные для занесения  коллекцию
-
-  if(!data.name || !data.link || !data.owner) {
-    const error = new Error("Переданы некорректные данные при создании карточки");
-    error.name = "Invalid parameters";
-    throw error;
-  }
-
-  data = {
-    ...data,
-    owner: req.user?._id
-  }
-
   try {
+    let data = req.body; //данные для занесения  коллекцию
+    console.log(data);
+    if(!data.name || !data.link) {
+      const error = new Error("Переданы некорректные данные при создании карточки");
+      error.name = "Invalid parameters";
+      throw error;
+    }
+
+    data = {
+      ...data,
+      owner: req.user?._id
+    }
+
     const newCard = await Card.create(data);
     return res.status(200).send(newCard);
   } catch (error) {
     if (error instanceof Error && error.name === "Invalid parameters") {
       return res.status(BAD_REQUEST).send({ message: error.message })
-    } else {
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "Ошибка на стороне сервера" })
     }
+
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(BAD_REQUEST).send({ message: error.message })
+    }
+
+    return res.status(INTERNAL_SERVER_ERROR).send({ message: "Ошибка на стороне сервера" })
   }
 }
 
@@ -51,17 +55,21 @@ export const deleteCardById = async (req: Request, res: Response) => {
 
     if (!deleteCard) {
       const error = new Error("Карточка с указанным _id не найдена");
-      error.name = "Invalid parameters";
+      error.name = "Not found";
       throw error;
     }
 
-    return res.status(200).send(`Карточка с id = ${cardId} удалена`)
+    return res.status(200).send({ message: `Карточка с id = ${cardId} удалена` })
   } catch (error) {
-    if (error instanceof Error && error.name === "Invalid parameters") {
+    if (error instanceof Error && error.name === "Not found") {
       return res.status(NOT_FOUND).send({ message: error.message })
-    } else {
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "Ошибка на стороне сервера" })
     }
+
+    if (error instanceof mongoose.Error.CastError) {
+      return res.status(BAD_REQUEST).send({ message: error.message })
+    }
+
+    return res.status(INTERNAL_SERVER_ERROR).send({ message: "Ошибка на стороне сервера" })
   }
 }
 
@@ -69,23 +77,20 @@ export const deleteCardById = async (req: Request, res: Response) => {
 export const likeCard = async (req: ITestRequest, res: Response) => {
   try {
     const { cardId } = req.params;
-    const ObjectID = require("mongodb").ObjectID
-
-    if (!cardId || !ObjectID.isValid(cardId)) {
-      const error = new Error("Передан несуществующий _id карточки");
-      error.name = "Invalid parameters _id card";
-      throw error;
-    }
-
     const likeCard = await Card.findByIdAndUpdate(
       cardId,
       { $addToSet: { likes: req.user?._id } },
       { new: true }
     );
 
-    return res.status(200).send(`like`)
+    if (!likeCard) {
+      const error = new Error("Карточка с указанным _id не найдена");
+      error.name = "Not found";
+      throw error;
+    }
+    return res.status(200).send({ message: `like` })
   } catch (error) {
-    if (error instanceof Error && error.name === "Invalid parameters _id card") {
+    if (error instanceof Error && error.name === "Not found") {
       return res.status(NOT_FOUND).send({ message: error.message })
     }
 
@@ -99,15 +104,7 @@ export const likeCard = async (req: ITestRequest, res: Response) => {
 
 //убрать лайк с карточки
 export const dislikeCard = async (req: ITestRequest, res: Response) => {
-  const { cardId } = req.params;
   const id = req.user?._id;
-  const ObjectID = require("mongodb").ObjectID
-
-  if (!cardId || !ObjectID.isValid(cardId)) {
-    const error = new Error("Передан несуществующий _id карточки");
-    error.name = "Invalid parameters _id card";
-    throw error;
-  }
 
   try {
     const dislikeCard = await Card.findByIdAndUpdate(
@@ -117,14 +114,14 @@ export const dislikeCard = async (req: ITestRequest, res: Response) => {
     )
 
     if (!dislikeCard) {
-      const error = new Error("Переданы некорректные данные для снятии лайка.");
-      error.name = "Invalid parameters";
+      const error = new Error("Карточка с указанным _id не найдена");
+      error.name = "Not found";
       throw error;
     }
 
-    return res.status(200).send(`delete like`)
+    return res.status(200).send({ message: `delete like` })
   } catch (error) {
-    if (error instanceof Error && error.name === "Invalid parameters _id card") {
+    if (error instanceof Error && error.name === "Not found") {
       return res.status(NOT_FOUND).send({ message: error.message })
     }
 
